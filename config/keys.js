1d730ce81ff8c768c833645188e4bdeb977e1778
Alexandre Froment <alexandrefroment@macbook-pro-de-alexandre.home>
2021-10-17 00:39:47 +0200

    first commit projecy

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://alexandrefroment:Tufila19@test-cluster.xfhuv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
